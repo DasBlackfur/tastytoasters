b0e4730b77b7274c21b507ca3ce4513ad68cde78
TheBlackfurGuy <64478051+TheBlackfurGuy@users.noreply.github.com>
2021-12-03 20:08:31 +0100

    Upgraded to 1.18

--- src/main/java/blackfur/tastytoasters/Tastytoasters.java
+++ src/main/java/blackfur/tastytoasters/Tastytoasters.java
@@ -1,8 +1,13 @@
 package blackfur.tastytoasters;
 
+import blackfur.tastytoasters.block.ToasterBlock;
 import blackfur.tastytoasters.items.*;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -13,6 +18,8 @@ public class Tastytoasters implements ModInitializer {
     public static final Item BURNT_TOAST_ITEM = new BurntToastItem();
     public static final Item BUTTER_ITEM = new ButterItem();
     public static final Item BUTTERED_TOAST_ITEM = new ButteredToastItem();
+
+    public static final ToasterBlock TOASTER_BLOCK = new ToasterBlock(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
     
     @Override
     public void onInitialize() {
@@ -21,5 +28,8 @@ public class Tastytoasters implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("tastytoasters", "burnt_toast"), BURNT_TOAST_ITEM);
         Registry.register(Registry.ITEM, new Identifier("tastytoasters", "butter"), BUTTER_ITEM);
         Registry.register(Registry.ITEM, new Identifier("tastytoasters", "buttered_toast"), BUTTERED_TOAST_ITEM);
+
+        Registry.register(Registry.BLOCK, new Identifier("tastytoasters", "toaster"), TOASTER_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("tastytoasters", "toaster"), new BlockItem(TOASTER_BLOCK, new Item.Settings().group(ItemGroup.FOOD)));
     }
 }
---
+++ src/main/java/blackfur/tastytoasters/block/ToasterBlock.java
@@ -0,0 +1,9 @@
+package blackfur.tastytoasters.block;
+
+import net.minecraft.block.Block;
+
+public class ToasterBlock extends Block {
+    public ToasterBlock(Settings settings) {
+        super(settings);
+    }
+}
