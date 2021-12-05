92352c24137a29ac8cb35cffd9dc66adebf0be2b
TheBlackfurGuy <64478051+TheBlackfurGuy@users.noreply.github.com>
2021-12-05 17:33:20 +0100

    I did a thing and nothing broke OMG

--- src/main/java/blackfur/tastytoasters/block/ToasterBlock.java
+++ src/main/java/blackfur/tastytoasters/block/ToasterBlock.java
@@ -1,20 +1,25 @@
 package blackfur.tastytoasters.block;
 
+import blackfur.tastytoasters.Tastytoasters;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.World;
 
-public class ToasterBlock extends HorizontalFacingBlock implements BlockEntityProvider {
+public class ToasterBlock extends BlockWithEntity implements BlockEntityProvider {
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
     public static final BooleanProperty TOASTING = BooleanProperty.of("toasting");
 
@@ -72,9 +77,27 @@ public class ToasterBlock extends HorizontalFacingBlock implements BlockEntityPr
         return null;
     }
 
-    @Nullable
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return null;
+        return new ToasterBlockEntity(pos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return checkType(type, Tastytoasters.TOASTER_BLOCK_ENTITY, ToasterBlockEntity::tick);
     }
 }
--- src/main/java/blackfur/tastytoasters/block/ToasterBlockEntity.java
+++ src/main/java/blackfur/tastytoasters/block/ToasterBlockEntity.java
@@ -3,10 +3,37 @@ package blackfur.tastytoasters.block;
 import blackfur.tastytoasters.Tastytoasters;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ToasterBlockEntity extends BlockEntity {
+    private int cookTicks;
+
     public ToasterBlockEntity(BlockPos pos, BlockState state) {
         super(Tastytoasters.TOASTER_BLOCK_ENTITY, pos, state);
+    }
+
+    public static void tick(World world, BlockPos pos, BlockState state, ToasterBlockEntity be) {
+
+    }
+
+    public void handleUse(PlayerEntity player, Hand hand, ItemStack itemStack) {
+
+    }
+
+    @Override
+    public void writeNbt(NbtCompound tag) {
+        super.writeNbt(tag);
+        tag.putInt("cookTime", cookTicks);
+    }
+
+    @Override
+    public void readNbt(NbtCompound tag) {
+        super.readNbt(tag);
+        cookTicks = tag.getInt("cookTime");
     }
 }
